ce654f080d54729acb3b2a6282ee85da09a64090
Ramsalunkhe12 <ramkrushnasalunkhe@gmail.com>
2023-12-04 16:11:46 +0530

    Icon chnage

--- src/components/forms/contact.jsx
+++ src/components/forms/contact.jsx
@@ -99,7 +99,7 @@ const ContactForm = () => {
                                 </div>
                                 <div className="dbox w-100 d-flex align-items-center">
                                     <div className="icon d-flex align-items-center justify-content-center">
-                                         <span className="fa-sharp fa-regular fa-envelope"></span>  
+                                         <span className="bi bi-envelope-fill"></span>  
                                     </div>
                                     <div className="text pl-3">
                                         <p><span >Email:</span> <a href="/cdn-cgi/l/email-protection#b5dcdbd3daf5ccdac0c7c6dcc1d09bd6dad8"><span className="__cf_email__" data-cfemail="9ef7f0f8f1dee7f1ebecedf7eafbb0fdf1f3">[email&#160;protected]</span></a></p>
