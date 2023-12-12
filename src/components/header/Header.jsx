a8af720a631183f606681c5e188e9f38631e2d9e
Ramsalunkhe12 <ramkrushnasalunkhe@gmail.com>
2023-12-12 19:15:31 +0530

    change navbar ,change shop_categorymain

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,18 +18,18 @@ const Header = () => {
             <div className="header-main">
                 <div className="container">
                     <div className="row header-wrapper justify-content-between align-items-center">
-                        <div className="col-2 pl-0">
+                        <div className="logo">
                             <a className="navbar-brand m-0 p-0" href="/">
-                                <img src="All_Images/logo (1).png" width="150" height="80" alt="site-logo" />
+                                <img src="#" alt="site-logo"    / >
                             </a>
                         </div>
-                        <div className="col-7">
+                        {/* <div className="col-7">
                             <div className="header-searchbar d-flex">
                                 <form id="product-searchbar" className="d-flex w-100" onSubmit={handleSubmit}>
                                     <div className="header-search-button col-1 pl-3 pr-0">
                                         <button type="submit"><i className="bi bi-search"></i></button>
-                                    </div>
-                                    <div className="header-search-input col-7 px-2">
+                                    </div> */}
+                                    {/* <div className="header-search-input col-7 px-2">
                                         <input
                                             type="search"
                                             placeholder="Search products..."
@@ -38,9 +38,9 @@ const Header = () => {
                                             value={productSearch}
                                             onChange={(event) => setProductSearch(event.target.value)}
                                             required
-                                        />
+                                        /> */}
                                     </div>
-                                    <div className="header-search-dropdown col-4 pl-0">
+                                    {/* <div className="header-search-dropdown col-4 pl-0">
                                         <select
                                             className="px-3"
                                             id="category-list"
@@ -54,20 +54,20 @@ const Header = () => {
                                             <option value="laptops">Laptops</option>
                                             <option value="accessories">Accessories</option>
                                         </select>
-                                    </div>
-                                </form>
+                                    </div> */}
+                                {/* </form> */}
                             </div>
                         </div>
                         <div className="col-auto pr-0 d-flex justify-content-end">
 
                             {/* NON LOGIN */}
 
-                            <div className="account-meta">
+                            {/* <div className="account-meta">
                                 <a href="" className="d-flex">
                                     <i className="bi bi-person"></i>
                                     <span>Log In</span>
                                 </a>
-                            </div>
+                            </div> */}
 
                             {/* LOGGED IN */}
 
@@ -81,9 +81,9 @@ const Header = () => {
                             */}
 
                         </div>
-                    </div>
-                </div>
-            </div>
+                    {/* </div> */}
+                {/* </div> */}
+            {/* </div> */}
         </>
     )
 }
